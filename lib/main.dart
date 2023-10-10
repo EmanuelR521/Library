c8628329d9b46ad60081ba1118bd1c8e6eff5bac
Emanuel Rios <emanuel.riosb@upb.edu.co>
2023-10-10 13:56:42 -0500

    feat: boton de ingreso redirige a menu principal

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor:const  Color(0xff220A05)),
         useMaterial3: true,
       ),
-      home: const MenuPrincipal(),
+      home: const LoginPage(),
     );
   }
 }
